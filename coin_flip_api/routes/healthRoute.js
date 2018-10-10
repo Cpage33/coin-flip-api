1a3171140eb047c2b53702fe8b1d5f7948647047
Conner Page <cpage@anderson.edu>
2018-10-09 20:16:13 -0400

    CoinFlipApi Funcitonal, will respond with correct json.

---
+++ coin_flip_api/routes/healthRoute.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * View/Route layer for healthcheck endpoint
+ */
+
+var express = require("express");
+var router = express.Router();
+
+/**
+ * Health check get @ http://localhost:5000/health
+ * returns json body w/ name & version
+ */
+router.get("/", (req, res) => {
+  let healthJson = {
+    name: "Conner's Coin Call",
+    version: "1.0.0"
+  };
+
+  res.json(healthJson);
+});
+
+module.exports = router;
